8c1d21e1f4604129dacfd2587c4c7107a12894dc
drrnyip <drrnyip@gmail.com>
2018-10-07 22:41:20 -0400

    question 4 answer

--- 4.js
+++ 4.js
@@ -4,3 +4,34 @@
 
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
+const checkIfPalindrome = (number) => {
+	const numberString = number.toString();
+	for (let i = 0; i < numberString.length; i++) {
+		if (i < numberString.length / 2 && numberString[i] != numberString[numberString.length - i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+const first = 999;
+const second = 999;
+
+let largestPalindrome = 0;
+
+let productOne = 0;
+let productTwo = 0;
+
+for (let i = first; i > 0; i--) {
+	for (let j = second; j > 0; j--) {
+		const product = i * j;
+		const res = checkIfPalindrome(product);
+		if (res && largestPalindrome < product) {
+			largestPalindrome = product;
+			productOne = i;
+			productTwo = j; 
+		}
+	}
+}
+
+console.log(`Largest palindrome: ${largestPalindrome} | Product of ${productOne} * ${productTwo}`);
