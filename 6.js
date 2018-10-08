9f753d358c95af2336226b0e1d697448a4b71cd5
drrnyip <drrnyip@gmail.com>
2018-10-07 23:31:25 -0400

    answer for 6

--- 6.js
+++ 6.js
@@ -2,11 +2,24 @@
 
 // The sum of the squares of the first ten natural numbers is,
 
-// 12 + 22 + ... + 102 = 385
+// 1**2 + 2**2 + ... + 10**2 = 385
 // The square of the sum of the first ten natural numbers is,
 
-// (1 + 2 + ... + 10)2 = 552 = 3025
+// (1 + 2 + ... + 10)**2 = 55**2 = 3025
 // Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
+const sumOfSquares = (n) => {
+	let sum = 0;
+	for (let i = 1; i <= n; i++) {
+		sum += i**2;
+	}
+	return sum;
+}
+
+const squareOfSums = (n) => {
+	return ((n / 2) * (1 + n))**2;
+}
+
+console.log(`Answer: ${squareOfSums(100) - sumOfSquares(100)}`);
