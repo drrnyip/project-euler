906e0d2595ba35562d47fffc8306066175db99e7
drrnyip <drrnyip@gmail.com>
2018-10-07 23:17:33 -0400

    answer for question 5

--- 5.js
+++ 5.js
@@ -4,3 +4,21 @@
 
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+const isDivisible = (number, target) => {
+	return (number % target == 0);
+}
+
+const high = 20;
+const low = 1;
+let answer = high;
+
+for (let i = high; i >= low; i--) {
+	if (!isDivisible(answer, i)) {
+		answer += high;
+		i = high;
+	}
+	if (i == low) {
+		console.log(`Answer: ${high} = ${answer}`)
+		return;
+	}
+}
